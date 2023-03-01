374807bcca849736f6cf844524be59a1d89f886e
Gabriel Puiu <Gabrielpuiu213@gmail.com>
2023-03-01 17:52:39 +0000

    add functions to ships and hidden and guess board

--- run.py
+++ run.py
@@ -1,6 +1,39 @@
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Hidden and Guess board.
+"""
+HIDDEN_BOARD = [[' '] * 8 for x in range(8)]
+GUESS_BOARD = [[' '] * 8 for x in range(8)]
 
-from colorama import init, Fore, Back, Style
-init()
+letters_to_numbers = {
+    'A': 0,
+    'B': 1,
+    'C': 2,
+    'D': 3,
+    'E': 4,
+    'F': 5,
+    'G': 6,
+    'H': 7
+}
 
+# functions
 
+  def print_board(board):
+    print("  A B C D E F G H")
+    print("  +-+-+-+-+-+-+-+")
+    row_number = 1
+    for row in board:
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_number += 1
+        
+  def create_ships():
+    pass
+
+def get_ship_location():
+    pass
+
+def count_hit_ships():
+    pass
+
+create_ships()
+
+turns = 10
